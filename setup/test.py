225c8d8bb03123278f91e1b99fc9512bfd86c352
wasit7 <wasit7@gmail.com>
2015-03-12 17:20:36 +0700

    test.py find submission.h

--- setup/test.py
+++ setup/test.py
@@ -1,13 +1,15 @@
 import os
+
 question_name="question1"
-submission="submission.h"
-home="home"
-for root, dirs, files in os.walk("/home"):
-    if root == "/home":
+submission_name="submission.h"
+home="/home"
+for root, dirs, files in os.walk(home):
+    if root == home:
         for user_dir in dirs:
     	    if user_dir != "ubuntu":
-                print "user_der:",user_dir
-                for subroot,subdirs,subfiles in os.walk(user_dir):
-                    if subdirs  == question_name:
-                        print 'question_dir:',q_subdir
-    	                print subfiles
+                print "user_der:",os.path.join(root,user_dir)
+                for subroot,subdirs,subfiles in os.walk(os.path.join(root,user_dir)):
+                    for subdir in subdirs:
+                        if subdir  == question_name:
+                            print "question path:",os.path.join(home, user_dir, subdir)
+                            print "file:",os.path.join(home, user_dir, subdir,submission_name)
