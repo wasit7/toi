2252ca0110b5d225824c67f852e3d0bc83d55657
wasit7 <wasit7@gmail.com>
2015-03-12 15:59:37 +0700

    ccrm.py oswalk

--- setup/ccrm.py
+++ setup/ccrm.py
@@ -10,7 +10,9 @@ Created on Thu Mar 12 15:33:56 2015
 #mark
 toi_dir="/home/ubuntu/toi2015"
 home_dir="/home"
-
+work_dir="home/"
 import os
 
-for root, dirs, files in os.walk():
+for root, user_dirs, files in os.walk(home_dir):
+    print user_dirs
+    
