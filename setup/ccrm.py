efe03d4b6def8c51bbb9b5755ba9de2b5ffa87be
wasit7 <wasit7@gmail.com>
2015-03-12 15:40:52 +0700

    add ccrm.py

---
+++ setup/ccrm.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 15:33:56 2015
+
+@author: Wasit
+"""
+#copy
+#complie
+#run
+#mark
+toi_dir="/home/ubuntu/toi2015"
+home_dir="/home"
+
+import os
+
+for root, dirs, files in os.walk():
