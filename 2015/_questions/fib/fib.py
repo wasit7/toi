0f68c708f52f5779cda74f260701911a15dcbf7f
wasit7 <wasit7@gmail.com>
2015-03-13 21:30:53 +0700

    first day

--- 2015/_questions/fib/fib.py
+++ 2015/_questions/fib/fib.py
@@ -5,7 +5,7 @@ import datetime
 
 question_name="fib"
 submission_name="submission.h"
-constrain=" -m 32000 -t 5 "
+constrain=" -m 32000 -t 3 "
 home_dir="/home"
 work_dir="/home/ubuntu/toi/2015"
 ms_dir="/home/ubuntu/toi/2015/_questions"
@@ -34,10 +34,11 @@ for root, dirs, files in os.walk(home_dir):
                     support_path=os.path.join(ms_dir,question_name)
                     cpp_path=os.path.join(ms_dir,question_name,"main.cpp")
                     out_path=os.path.join(dst_dir,"main.out")
-                    log_path=os.path.join(dst_dir,"log_%s"%(datetime.datetime.now().strftime("%d_%H%M%S")))
+                    #log_path=os.path.join(dst_dir,"make_log_%s.txt"%(datetime.datetime.now().strftime("%d_%H%M%S")))
+                    log_path=os.path.join(dst_dir,"error.log")
                     print "--compiling: support:",support_path
                     print "            cpp_path:",cpp_path
-                    the_cmd="g++ -std=c++11 -Wall -I %s -I %s -o %s %s > %s 2>&1"%(dst_dir,support_path,out_path,cpp_path,log_path)
+                    the_cmd="g++ -std=c++11 -Wall -I %s -I %s -o %s %s >%s 2>&1"%(dst_dir,support_path,out_path,cpp_path,log_path)
                     #print the_cmd
                     os.system(the_cmd)
 
@@ -47,8 +48,9 @@ for root, dirs, files in os.walk(home_dir):
                     #markschem
                     ms_file_path=os.path.join(ms_dir,question_name,"file_out.txt") 
                     #student result
-                    out_log_file=os.path.join(dst_dir,"out_%s"%(datetime.datetime.now().strftime("%d_%H%M%S")))
-                    the_cmd=  os.path.join(work_dir,"src/timeout.sh --detect-hangups -p '.*main.out.*,TOIMAIN.OUT' ")+constrain+ out_path + " " + in_file_path + " " + ms_file_path + "> %s 2>&1"%(out_log_file)
+                    #out_log_file=os.path.join(dst_dir,"out_%s"%(datetime.datetime.now().strftime("%d_%H%M%S")))
+                    out_log_file=os.path.join(dst_dir,"out.log")
+                    the_cmd=  os.path.join(work_dir,"src/timeout.sh --detect-hangups -p '.*main.out.*,MAIN.OUT' ")+constrain+ out_path + " " + in_file_path + " " + ms_file_path + " >%s 2>&1"%(out_log_file)
                     print "--running:",the_cmd
                     os.system(the_cmd)
 
